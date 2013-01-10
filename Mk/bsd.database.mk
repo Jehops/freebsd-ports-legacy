Jehops/freebsd-ports-legacy

commit 056de6a8922e231f934d6b33f749c4c0c94f5c4f

Add the 5.5 branch of MariaDB
PR:		ports/174660
Submitted by:	Alexandr Kovalenko <never@nevermind.kiev.ua>
Approved by:	geoffroy desvernay <dgeo@centrale-marseille.fr>
		(maintainer of databases/mariadb-*)


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@309955 aed309b6-a8cd-e111-996c-001c23d10e55

Mk/bsd.database.mk
@@ -111,15 +111,19 @@ MYSQL51_LIBVER=		16
 MYSQL53_LIBVER=		16
 MYSQL55_LIBVER=		18
 MYSQL55p_LIBVER=	18
+MYSQL55m_LIBVER=	18
 MYSQL56p_LIBVER=	18
 
 # Setting/finding MySQL version we want.
 .if exists(${LOCALBASE}/bin/mysql)
 _MYSQL!=	${LOCALBASE}/bin/mysql --version | ${SED} -e 's/.*Distrib \([0-9]\)\.\([0-9]*\).*/\1\2/'
 _PERCONA!=	${LOCALBASE}/bin/mysql --version | ${GREP} Percona | wc -l
+_MARIADB!=	${LOCALBASE}/bin/mysql --version | ${GREP} MariaDB | wc -l
 
 .if ${_PERCONA} == 1
 _MYSQL_VER=	${_MYSQL}p
+.elif ${_MARIADB} == 1
+_MYSQL_VER=	${_MYSQL}m
 .else
 _MYSQL_VER=	${_MYSQL}
 .endif
@@ -152,6 +156,9 @@ _MYSQL_SERVER=	databases/mariadb-server
 .elif (${MYSQL_VER} == "55p")
 _MYSQL_CLIENT=	databases/percona55-client
 _MYSQL_SERVER=	databases/percona55-server
+.elif (${MYSQL_VER} == "55m")
+_MYSQL_CLIENT=	databases/mariadb55-client
+_MYSQL_SERVER=	databases/mariadb55-server
 .elif (${MYSQL_VER} == "56p")
 _MYSQL_CLIENT=	databases/percona56-client
 _MYSQL_SERVER=	databases/percona56-server

databases/Makefile
@@ -188,6 +188,8 @@
     SUBDIR += mariadb-client
     SUBDIR += mariadb-scripts
     SUBDIR += mariadb-server
+    SUBDIR += mariadb55-client
+    SUBDIR += mariadb55-server
     SUBDIR += mdbtools
     SUBDIR += mdcached
     SUBDIR += memcached

databases/mariadb55-client/Makefile
@@ -1,0 +1,28 @@
+# $FreeBSD$
+
+PORTNAME=	mariadb
+PKGNAMESUFFIX=	-client
+
+COMMENT=	Multithreaded SQL database (client)
+
+MASTERDIR=	${.CURDIR}/../mariadb55-server
+
+PKGMESSAGE=	mustnotexist
+PATCHDIR=	${.CURDIR}/files
+PLIST=		${.CURDIR}/pkg-plist
+
+LATEST_LINK=	mariadb55-client
+
+CONFLICTS_INSTALL=	mysql-client-[345].* mariadb-client-5.3.* percona-client-5.*
+
+CMAKE_ARGS+=	-DWITHOUT_SERVER=1
+
+USE_LDCONFIG=	${PREFIX}/lib/mysql
+
+MAN1=		msql2mysql.1 mysql.1 mysql_config.1 mysql_find_rows.1 \
+		mysql_waitpid.1 mysqlaccess.1 mysqladmin.1 mysqlbinlog.1 \
+		mysqlcheck.1 mysqldump.1 mysqlimport.1 mysqlshow.1 mysqlslap.1
+
+CLIENT_ONLY=	yes
+
+.include "${MASTERDIR}/Makefile"

databases/mariadb55-server/Makefile
@@ -1,0 +1,106 @@
+# $FreeBSD$
+
+PORTNAME?=	mariadb
+PORTVERSION=	5.5.28a
+PORTREVISION?=	0
+CATEGORIES=	databases ipv6
+MASTER_SITES=	http://ftp.osuosl.org/pub/mariadb/${PORTNAME}-${PORTVERSION}/kvm-tarbake-jaunty-x86/ \
+		http://mirrors.supportex.net/mariadb/${PORTNAME}-${PORTVERSION}/kvm-tarbake-jaunty-x86/ \
+		http://mirror.aarnet.edu.au/pub/MariaDB/${PORTNAME}-${PORTVERSION}/kvm-tarbake-jaunty-x86/ \
+		http://mirror2.hs-esslingen.de/pub/Mirrors/mariadb/${PORTNAME}-${PORTVERSION}/kvm-tarbake-jaunty-x86/ \
+		http://gd.tuwien.ac.at/db/mariadb/${PORTNAME}-${PORTVERSION}/kvm-tarbake-jaunty-x86/ \
+		http://mirrors.fe.up.pt/pub/mariadb/${PORTNAME}-${PORTVERSION}/kvm-tarbake-jaunty-x86/ \
+		http://mirror.de.gsnw.de:56431/mariadb/${PORTNAME}-${PORTVERSION}/kvm-tarbake-jaunty-x86/ \
+		http://mirror.layerjet.com/mariadb/${PORTNAME}-${PORTVERSION}/kvm-tarbake-jaunty-x86/ \
+		http://mirror.switch.ch/mirror/mariadb/${PORTNAME}-${PORTVERSION}/kvm-tarbake-jaunty-x86/
+PKGNAMESUFFIX?=	-server
+
+MAINTAINER=	never@nevermind.kiev.ua
+COMMENT?=	Multithreaded SQL database (server)
+
+SLAVEDIRS=	databases/mariadb55-client
+USE_CMAKE=	yes
+MAKE_JOBS_SAFE=	yes
+NO_OPTIONS_SORT=yes
+
+OPTIONS_DEFINE=	SSL FASTMTX
+OPTIONS_DEFAULT=SSL
+
+FASTMTX_DESC=	Replace mutexes with spinlocks
+
+CMAKE_ARGS+=	-DINSTALL_DOCDIR="share/doc/mysql" \
+		-DINSTALL_DOCREADMEDIR="share/doc/mysql" \
+		-DINSTALL_INCLUDEDIR="include/mysql" \
+		-DINSTALL_INFODIR="info" \
+		-DINSTALL_LIBDIR="lib/mysql" \
+		-DINSTALL_MANDIR="man" \
+		-DINSTALL_MYSQLDATADIR="/var/db/mysql" \
+		-DINSTALL_MYSQLSHAREDIR="share/mysql" \
+		-DINSTALL_MYSQLTESTDIR="share/mysql/tests" \
+		-DINSTALL_PLUGINDIR="lib/mysql/plugin" \
+		-DINSTALL_SBINDIR="libexec" \
+		-DINSTALL_SCRIPTDIR="bin" \
+		-DINSTALL_SHAREDIR="share" \
+		-DINSTALL_SQLBENCHDIR="share/mysql" \
+		-DINSTALL_SUPPORTFILESDIR="share/mysql" \
+		-DWITH_LIBEDIT=0 \
+		-DWITH_LIBWRAP=1
+
+.ifdef USE_MYSQL
+.error You have `USE_MYSQL' variable defined either in environment or in make(1) arguments. Please undefine and try again.
+.endif
+
+DATADIR=	${PREFIX}/share/mysql
+USE_LDCONFIG=	${PREFIX}/lib/mysql
+
+.include <bsd.port.options.mk>
+
+.if ${PORT_OPTIONS:MSSL}
+CMAKE_ARGS+=	-DWITH_SSL=bundled
+.endif
+.if ${PORT_OPTIONS:MFASTMTX}
+CMAKE_ARGS+=	-DWITH_FAST_MUTEXES=1
+.endif
+
+# MySQL-Server part
+.if !defined(CLIENT_ONLY)
+USE_MYSQL=	yes
+WANT_MYSQL_VER=	55m
+
+LATEST_LINK=	mariadb55-server
+
+CONFLICTS_INSTALL=	mysql-server-[345].* mariadb-server-5.3.* percona-server-5.*
+
+USE_RC_SUBR=	mysql-server
+
+USERS=		mysql
+GROUPS=		mysql
+
+MAN1=		my_print_defaults.1 myisam_ftdump.1 myisamchk.1 myisamlog.1 myisampack.1 \
+		mysql.server.1 mysql_convert_table_format.1 mysql_fix_extensions.1 \
+		mysql_install_db.1 mysql_secure_installation.1 mysql_setpermission.1 \
+		mysql_tzinfo_to_sql.1 mysql_upgrade.1 mysql_zap.1 mysqlbug.1 \
+		mysqld_multi.1 mysqld_safe.1 mysqldumpslow.1 mysqlhotcopy.1 mysqlman.1 \
+		mysqltest.1 perror.1 replace.1 resolve_stack_dump.1 resolveip.1 \
+		innochecksum.1
+
+MAN8=		mysqld.8
+
+CMAKE_ARGS+=	-DWITH_EMBEDDED_SERVER="ON"
+
+post-install:
+.if !defined(PACKAGE_BUILDING)
+	@${CAT} ${PKGMESSAGE}
+.endif
+.endif
+
+post-patch:
+	@${REINPLACE_CMD} 's/*.1/${MAN1}/' ${WRKSRC}/man/CMakeLists.txt
+
+.include <bsd.port.pre.mk>
+
+.if ${ARCH} == "arm"
+BROKEN=		Does not compile on arm
+.endif
+
+.include <bsd.port.post.mk>

databases/mariadb55-client/files/patch-extra_yassl_taocrypt_src_integer.cpp
@@ -1,0 +1,11 @@
+--- extra/yassl/taocrypt/src/integer.cpp.orig	2012-12-17 03:41:47.000000000 +0200
++++ extra/yassl/taocrypt/src/integer.cpp	2012-12-17 03:42:47.000000000 +0200
+@@ -73,7 +73,7 @@
+ CPP_TYPENAME AlignedAllocator<T>::pointer AlignedAllocator<T>::allocate(
+                                            size_type n, const void *)
+ {
+-    if (n > max_size())
++    if (n > AlignedAllocator<T>::max_size())
+         return 0;
+     if (n == 0)
+         return 0;

databases/mariadb55-client/files/patch-include_my_compare.h
@@ -1,0 +1,11 @@
+--- include/my_compare.h.orig	2012-12-17 03:52:37.000000000 +0200
++++ include/my_compare.h	2012-12-17 03:53:40.000000000 +0200
+@@ -40,7 +40,7 @@
+   But beware the dependency of MI_MAX_POSSIBLE_KEY_BUFF and HA_MAX_KEY_LENGTH.
+ */
+ 
+-#define HA_MAX_KEY_LENGTH           1000        /* Max length in bytes */
++#define HA_MAX_KEY_LENGTH           4000        /* Max length in bytes */
+ #define HA_MAX_KEY_SEG              32          /* Max segments for key */
+ 
+ #define HA_MAX_POSSIBLE_KEY_BUFF    (HA_MAX_KEY_LENGTH + 24+ 6+6)

databases/mariadb55-client/files/patch-mysys_default.c
@@ -1,0 +1,28 @@
+--- mysys/default.c.orig	2012-12-17 03:57:40.000000000 +0200
++++ mysys/default.c	2012-12-17 04:00:28.000000000 +0200
+@@ -89,7 +89,7 @@
+ 
+ /* Which directories are searched for options (and in which order) */
+ 
+-#define MAX_DEFAULT_DIRS 6
++#define MAX_DEFAULT_DIRS 7
+ #define DEFAULT_DIRS_SIZE (MAX_DEFAULT_DIRS + 1)  /* Terminate with NULL */
+ static const char **default_directories = NULL;
+ 
+@@ -785,7 +785,7 @@
+   {
+     MY_STAT stat_info;
+     if (!my_stat(name,&stat_info,MYF(0)))
+-      return 1;
++      return 0;
+     /*
+       Ignore world-writable regular files.
+       This is mainly done to protect us to not read a file created by
+@@ -1229,6 +1229,7 @@
+ #if defined(DEFAULT_SYSCONFDIR)
+   if (DEFAULT_SYSCONFDIR[0])
+     errors += add_directory(alloc, DEFAULT_SYSCONFDIR, dirs);
++    errors += add_directory(alloc, DEFAULT_SYSCONFDIR "/mysql", dirs);
+ #endif /* DEFAULT_SYSCONFDIR */
+ 
+ #endif

databases/mariadb55-client/pkg-plist
@@ -1,0 +1,75 @@
+bin/msql2mysql
+bin/mysql
+bin/mysql_config
+bin/mysql_find_rows
+bin/mysql_waitpid
+bin/mysqlaccess
+bin/mysqladmin
+bin/mysqlbinlog
+bin/mysqlcheck
+bin/mysqldump
+bin/mysqlimport
+bin/mysqlshow
+bin/mysqlslap
+include/mysql/auth_dialog_client.h
+include/mysql/client_plugin.h
+include/mysql/decimal.h
+include/mysql/errmsg.h
+include/mysql/handler_state.h
+include/mysql/keycache.h
+include/mysql/m_ctype.h
+include/mysql/m_string.h
+include/mysql/ma_dyncol.h
+include/mysql/my_alloc.h
+include/mysql/my_attribute.h
+include/mysql/my_compiler.h
+include/mysql/my_config.h
+include/mysql/my_dbug.h
+include/mysql/my_decimal_limits.h
+include/mysql/my_dir.h
+include/mysql/my_getopt.h
+include/mysql/my_global.h
+include/mysql/my_list.h
+include/mysql/my_net.h
+include/mysql/my_pthread.h
+include/mysql/my_sys.h
+include/mysql/my_valgrind.h
+include/mysql/my_xml.h
+include/mysql/mysql.h
+include/mysql/mysql_com.h
+include/mysql/mysql_embed.h
+include/mysql/mysql_time.h
+include/mysql/mysql_version.h
+include/mysql/mysqld_ername.h
+include/mysql/mysqld_error.h
+include/mysql/plugin.h
+include/mysql/plugin_audit.h
+include/mysql/plugin_auth.h
+include/mysql/plugin_auth_common.h
+include/mysql/plugin_ftparser.h
+include/mysql/psi/mysql_file.h
+include/mysql/psi/mysql_thread.h
+include/mysql/psi/psi.h
+include/mysql/service_debug_sync.h
+include/mysql/service_my_snprintf.h
+include/mysql/service_progress_report.h
+include/mysql/service_thd_alloc.h
+include/mysql/service_thd_wait.h
+include/mysql/service_thread_scheduler.h
+include/mysql/services.h
+include/mysql/sql_common.h
+include/mysql/sql_state.h
+include/mysql/sslopt-case.h
+include/mysql/sslopt-longopts.h
+include/mysql/sslopt-vars.h
+include/mysql/typelib.h
+lib/mysql/libmysqlclient.a
+lib/mysql/libmysqlclient.so
+lib/mysql/libmysqlclient.so.18
+lib/mysql/libmysqlclient_r.a
+lib/mysql/libmysqlclient_r.so
+lib/mysql/libmysqlclient_r.so.18
+share/aclocal/mysql.m4
+@dirrm include/mysql/psi
+@dirrm include/mysql
+@dirrmtry lib/mysql

commit 06c86f1a8fe74cce44966cd60a80c60713e89bf3

- fix databases/mariadb-{server,client} by adding MYSQL_VER 53m. This was   broken by r309955. [1] - remove MYSQL_VER 53, database/mariadb-* where the only ports to use it. - sort
PR:		ports/175180
Submitted by:	Geoffroy Desvernay <dgeo@centrale-marseille.fr>
		Jeff Wheelhouse


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@310210 aed309b6-a8cd-e111-996c-001c23d10e55

Mk/bsd.database.mk
@@ -108,10 +108,10 @@ DEFAULT_MYSQL_VER?=	55
 MYSQL41_LIBVER=		14
 MYSQL50_LIBVER=		15
 MYSQL51_LIBVER=		16
-MYSQL53_LIBVER=		16
+MYSQL53m_LIBVER=	16
 MYSQL55_LIBVER=		18
-MYSQL55p_LIBVER=	18
 MYSQL55m_LIBVER=	18
+MYSQL55p_LIBVER=	18
 MYSQL56p_LIBVER=	18
 
 # Setting/finding MySQL version we want.
@@ -150,15 +150,15 @@ IGNORE=		cannot install: MySQL versions mismatch: mysql${_MYSQL_VER}-client is i
 .endif
 .endif
 
-.if (${MYSQL_VER} == "53")
+.if (${MYSQL_VER} == "53m")
 _MYSQL_CLIENT=	databases/mariadb-client
 _MYSQL_SERVER=	databases/mariadb-server
-.elif (${MYSQL_VER} == "55p")
-_MYSQL_CLIENT=	databases/percona55-client
-_MYSQL_SERVER=	databases/percona55-server
 .elif (${MYSQL_VER} == "55m")
 _MYSQL_CLIENT=	databases/mariadb55-client
 _MYSQL_SERVER=	databases/mariadb55-server
+.elif (${MYSQL_VER} == "55p")
+_MYSQL_CLIENT=	databases/percona55-client
+_MYSQL_SERVER=	databases/percona55-server
 .elif (${MYSQL_VER} == "56p")
 _MYSQL_CLIENT=	databases/percona56-client
 _MYSQL_SERVER=	databases/percona56-server

databases/mariadb-server/Makefile
@@ -28,7 +28,7 @@ GNU_CONFIGURE=	yes
 
 .if !defined(MARIADB_CLIENT_SLAVE)
 USE_MYSQL=	yes
-WANT_MYSQL_VER=	53
+WANT_MYSQL_VER=	53m
 .endif
 
 .if !defined(MARIADB_SCRIPTS_SLAVE)
@@ -42,7 +42,7 @@ GROUPS=		mysql
 
 OPTIONS_DEFINE+=	STATIC SSL OPENSSL
 OPTIONS_DEFAULT+=	SSL
-STATIC_DESC=	Build statically linked executables (not on 9.x)
+STATIC_DESC=	Statically link executables (not on 9.x)
 SSL_DESC=	Activate SSL support
 OPENSSL_DESC=	Use OpenSSL instead of bundled yassl
 