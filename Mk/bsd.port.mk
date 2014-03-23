Jehops/freebsd-ports-legacy

commit 26692e5b9fbb0ea4e3d151a1ade6ac1fa7ae39b2

install-package: Use WRKDIR/PKGNAME.PKG_SUFX if available
With hat:	portmgr


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@348859 35697150-7ecd-e111-bb59-0022644237b5

Mk/bsd.port.mk
@@ -3835,13 +3835,17 @@ delete-package-list: delete-package-links-list
 	@${ECHO_CMD} "[ -f ${PKGFILE} ] && (${ECHO_CMD} deleting ${PKGFILE}; ${RM} -f ${PKGFILE})"
 .endif
 
-# Only used if !defined(NO_STAGE)
+# Used if !defined(NO_STAGE) during install, or manually to install package
+# from local repository.
 .if !target(install-package)
-install-package:
 .if defined(FORCE_PKG_REGISTER)
-	@${PKG_ADD} -f ${WRKDIR}/${PKGNAME}${PKG_SUFX}
+_INSTALL_PKG_ARGS=	-f
+.endif
+install-package:
+.if exists(${WRKDIR}/${PKGNAME}${PKG_SUFX})
+	@${PKG_ADD} ${_INSTALL_PKG_ARGS} ${WRKDIR}/${PKGNAME}${PKG_SUFX}
 .else
-	@${PKG_ADD} ${WRKDIR}/${PKGNAME}${PKG_SUFX}
+	@${PKG_ADD} ${_INSTALL_PKG_ARGS} ${PKGFILE}
 .endif
 .endif
 

commit 13af83fb2a2cba21ac74ffac952300106fd875b3

- Fix installing with pkg_install when running 'make install' due to the   exists() being evaluated before the package is created.
  Regression from r348859

 With hat:	portmgr
 Pointyhat to:	bdrewery


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@348880 35697150-7ecd-e111-bb59-0022644237b5

Mk/bsd.port.mk
@@ -3843,11 +3843,12 @@ delete-package-list: delete-package-links-list
 _INSTALL_PKG_ARGS=	-f
 .endif
 install-package:
-.if exists(${WRKDIR}/pkg/${PKGNAME}${PKG_SUFX})
-	@${PKG_ADD} ${_INSTALL_PKG_ARGS} ${WRKDIR}/pkg/${PKGNAME}${PKG_SUFX}
-.else
-	@${PKG_ADD} ${_INSTALL_PKG_ARGS} ${PKGFILE}
-.endif
+	@if [ -f "${WRKDIR}/pkg/${PKGNAME}${PKG_SUFX}" ]; then \
+	    _pkgfile="${WRKDIR}/pkg/${PKGNAME}${PKG_SUFX}"; \
+	else \
+	    _pkgfile="${PKGFILE}"; \
+	fi; \
+	${PKG_ADD} ${_INSTALL_PKG_ARGS} $${_pkgfile}
 .endif
 
 