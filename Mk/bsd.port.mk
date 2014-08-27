Jehops/freebsd-ports-legacy

commit e81a7f149e107684df791236bb4e279131f7eed9

Bring net-mgmt/netmond back into shape.
From PR submitter:
- Fix build (partially)
- Clean up patches
- Unexpire
- Mark jobs unsafe
- Bump PORTREVISION

From myself:
- Stage support, introduce pkg-plist
- Use option helpers
- Convert patch site for documentation into an optional distribution file

PR:		ports/186654
Submitted by:	Vsevolod Volkov <vvv@colocall.net>


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@347404 35697150-7ecd-e111-bb59-0022644237b5

net-mgmt/netmond/Makefile
@@ -3,78 +3,51 @@
 
 PORTNAME=	netmond
 PORTVERSION=	2.2b6
-PORTREVISION=	5
+PORTREVISION=	6
 CATEGORIES=	net-mgmt
-MASTER_SITES=	http://freebsd.unixfreunde.de/sources/ \
-		ftp://ftp.risp.ru/pub/RinetSoftware/
-DISTNAME=	netmond-2.2-b6
+MASTER_SITES=	http://freebsd.unixfreunde.de/sources/:daemon \
+		ftp://ftp.risp.ru/pub/RinetSoftware/:daemon \
+		http://vfom.narod.ru/download/:docs
 EXTRACT_SUFX=	.tgz
-
-PATCH_SITES=	http://freebsd.unixfreunde.de/sources/ \
-		http://vfom.narod.ru/download/
-PATCH_VERSION=	1
-PATCHFILES=	${DISTNAME}.patch${PATCH_VERSION}${EXTRACT_SUFX}  ${DISTNAME}.docs${EXTRACT_SUFX}
+DISTFILES=	${PORTNAME}-2.2-b6${EXTRACT_SUFX}:daemon
+EXTRACT_ONLY=	${PORTNAME}-2.2-b6${EXTRACT_SUFX}
 
 MAINTAINER=	ports@FreeBSD.org
 COMMENT=	Netmond - IP network monitoring daemon
 
+WRKSRC=		${WRKDIR}/${PORTNAME}-2.2-b6
+
 USERS=	netmon
 GROUPS=	${USERS}
 
-BROKEN=		Does not patch
-DEPRECATED=	Broken for more than 6 month
-EXPIRATION_DATE=	2014-02-27
-
-#MK_IFGRAPH=	yes
-PATCHDIR=	${WRKDIR}/patches
-
 GNU_CONFIGURE=	yes
+DISABLE_MAKE_JOBS=yes
 USES=		bison
 CFLAGS+=	-Wno-return-type
 
-BINOWN=		root
-BINGRP=		netmon
-BINMODE=	0550
-
-PLIST_FILES=	sbin/netmond sbin/netmond_watchdog sbin/netmondctl \
-		etc/netmond.conf.sample
 USE_RC_SUBR=	netmond
 SUB_FILES+=	pkg-message netmond_watchdog netmondctl
-PORTDOCS=	README README.ru CHANGES README.port.eng README.port.ru index.html netmond.css en/ ru/
+PORTDOCS=	README README.ru CHANGES README.port.eng README.port.ru index.html netmond.css en/* ru/*
 
 OPTIONS_DEFINE=	DOCS GD
+OPTIONS_SUB=	yes
 
-NO_STAGE=	yes
+GD_LIB_DEPENDS=	libgd.so:${PORTSDIR}/graphics/gd
+GD_CONFIGURE_OFF=	--without-ifgraph
+
+DOCS_DISTFILES= ${PORTNAME}-2.2-b6.docs${EXTRACT_SUFX}:docs
+
 .include <bsd.port.options.mk>
 
-.if ${PORT_OPTIONS:MGD}
-LIB_DEPENDS=	gd:${PORTSDIR}/graphics/gd
-PLIST_FILES+=	sbin/ifgraph
-.else
-CONFIGURE_ARGS=	--without-ifgraph
-.endif
-
-post-extract:
-	${MKDIR} ${PATCHDIR}
-	${TAR} -zxf ${DISTDIR}/${DISTNAME}.patch${PATCH_VERSION}${EXTRACT_SUFX} -C ${PATCHDIR}
-	${CP} ${PATCHDIR}/pipe.c ${WRKSRC}
-
 do-install:
-	${INSTALL_PROGRAM} ${WRKSRC}/netmond      ${PREFIX}/sbin/netmond
-.if defined(MK_IFGRAPH)
-	${INSTALL_PROGRAM} ${WRKSRC}/ifgraph      ${PREFIX}/sbin/ifgraph
+	${INSTALL_PROGRAM} ${WRKSRC}/netmond ${STAGEDIR}${PREFIX}/sbin/netmond
+.if ${PORT_OPTIONS:MGD}
+	${INSTALL_PROGRAM} ${WRKSRC}/ifgraph ${STAGEDIR}${PREFIX}/sbin/ifgraph
 .endif
-	${INSTALL_SCRIPT}   ${WRKDIR}/netmond_watchdog ${PREFIX}/sbin/netmond_watchdog
-	${INSTALL_SCRIPT}   ${WRKDIR}/netmondctl ${PREFIX}/sbin/netmondctl
-	${INSTALL_DATA} ${WRKSRC}/netmon.conf.sample ${PREFIX}/etc/netmond.conf.sample
-	${CHMOD} u+s ${PREFIX}/sbin/netmond
-.if ${PORT_OPTIONS:MDOCS}
-	@${MKDIR}  ${DOCSDIR}
-	${CHMOD} 755 ${DOCSDIR}
-	${TAR}  -zxf ${DISTDIR}/${DISTNAME}.docs${EXTRACT_SUFX} -C ${DOCSDIR}
-.endif
-
-post-install:
-	${CAT} ${PKGMESSAGE}
+	${INSTALL_SCRIPT} ${WRKDIR}/netmond_watchdog ${STAGEDIR}${PREFIX}/sbin/netmond_watchdog
+	${INSTALL_SCRIPT} ${WRKDIR}/netmondctl ${STAGEDIR}${PREFIX}/sbin/netmondctl
+	${INSTALL_DATA} ${WRKSRC}/netmon.conf.sample ${STAGEDIR}${PREFIX}/etc/netmond.conf.sample
+	@${MKDIR} ${STAGEDIR}${DOCSDIR}
+	${TAR} -zxf ${DISTDIR}/${PORTNAME}-2.2-b6.docs${EXTRACT_SUFX} -C ${STAGEDIR}${DOCSDIR}
 
 .include <bsd.port.mk>

net-mgmt/netmond/pkg-plist
@@ -1,0 +1,12 @@
+@group netmon
+@mode 0550
+%%GD%%sbin/ifgraph
+sbin/netmond
+sbin/netmond_watchdog
+@mode u+s
+sbin/netmondctl
+@mode
+@group wheel
+etc/netmond.conf.sample
+%%PORTDOCS%%@dirrmtry %%DOCSDIR%%/ru
+%%PORTDOCS%%@dirrmtry %%DOCSDIR%%/en

net-mgmt/netmond/distinfo
@@ -1,6 +1,4 @@
 SHA256 (netmond-2.2-b6.tgz) = 2b31096f731e347b65ce2c2a11f1ce70af0d8d7a9ff0c49edf4b3c3b65d0befd
 SIZE (netmond-2.2-b6.tgz) = 264500
-SHA256 (netmond-2.2-b6.patch1.tgz) = 0fa7aae8dcfce5ba735af2ec0dfd3fc7a7d4e991cf2dc331ed6714e3ad9d08e5
-SIZE (netmond-2.2-b6.patch1.tgz) = 18574
 SHA256 (netmond-2.2-b6.docs.tgz) = 4cdad1003455c99ee1d3067a4db7a01f1db11247d2d5fe683c384f6d05ed79e8
 SIZE (netmond-2.2-b6.docs.tgz) = 110692

net-mgmt/netmond/files/patch-Makefile.in
@@ -1,11 +1,0 @@
---- Makefile.in.orig	Tue Jul 20 19:28:20 2004
-+++ Makefile.in	Thu Nov  2 13:35:27 2006
-@@ -47,7 +47,7 @@
- 
- NETMOND_C = netmond.c netstate.c event.c session.c mib.c snmp.c router.c \
- 	trap.c ping.c tcp.c udp.c dns.c radius.c tacacs.c md5.c util.c \
--	variables.c save.c regex.c malloc.c reconfig.c
-+	variables.c save.c regex.c malloc.c reconfig.c pipe.c
- NETMOND_Y = calc.y parseconf.y
- NETMOND_L = scanconf.l
- NETMOND_G = version.c

net-mgmt/netmond/files/patch-dns.c
@@ -1,54 +1,0 @@
---- dns.c.orig	Mon Aug 25 18:19:04 2003
-+++ dns.c	Thu Nov  2 13:35:27 2006
-@@ -149,6 +149,8 @@
- {
- 	SESSION *sd = method->sd;
- 	int reqid;
-+	struct sockaddr_in *from;
-+	char ipaddr[20];
- 
- 	/* sanity check */
- 	if (!sd) return;
-@@ -161,6 +163,14 @@
- 		return;
- 	}
- 
-+	/* bind  socket to local source address */
-+
-+	from = (struct sockaddr_in *)&sd->me;
-+	if ( from->sin_addr.s_addr != INADDR_ANY ) {		
-+	    if( bind(sd->sock, &sd->me, sizeof(struct sockaddr) ) == -1 ) 
-+		report(LOG_WARNING, "dns_start : bind failed for %s: %s",
-+		    	intoa(ipaddr,from->sin_addr), strerror(*(__error())) );
-+	}
- 	/* turn on non-blocking I/O */
- 	if (set_socket_async(sd->sock, TRUE) < 0) {
- 		dns_reply(errno, sd, 0);
-@@ -288,7 +298,7 @@
- 	METHOD *method;
- {
- 	SESSION template;
--	struct sockaddr_in *to;
-+	struct sockaddr_in *to, *from;
- 
- 	dprintf(("dns_init(%s/%s)\n", target->name, method->name));
- 
-@@ -303,6 +313,10 @@
- 	to->sin_family = AF_INET;
- 	to->sin_port = htons(method->rport);
- 	to->sin_addr = method->address ? method->ip_addr : target->ip_addr;
-+	from = (struct sockaddr_in *)&template.me;
-+	bzero((char *)from, sizeof(struct sockaddr_in));
-+	from->sin_family = AF_INET;
-+	from->sin_addr = target->ip_srcaddr;
- 	template.timeout = method->timeout * 1000000L; /* make microseconds */
- 	template.retries = method->retries;
- 	template.send = dns_send;
-@@ -332,6 +346,7 @@
- 	IPPROTO_UDP,	/* network protocol */
- 	NAMESERVER_PORT, /* server port */
- 	0, 0,		/* timeout and retries undefined yet */
-+        NULL,NULL,       /* when variables unused */
- 	{ 0, 0 },	/* no parameters used */
- 
- 	/* Non-initialized data */

net-mgmt/netmond/files/patch-event.c
@@ -1,11 +1,0 @@
---- event.c.orig	Thu Feb 27 17:42:32 2003
-+++ event.c	Tue Nov 14 17:40:52 2006
-@@ -288,7 +288,7 @@
- #ifdef	HAVE_PTHREAD
- 	pthread_mutex_lock(&localtime_lock);
- #endif
--	tm = localtime(&tvp->tv_sec);
-+	tm = localtime((time_t *)&tvp->tv_sec);
- 	defect = tm->tm_sec + 60 * tm->tm_min + 3600 * tm->tm_hour + off;
- #ifdef	HAVE_PTHREAD
- 	pthread_mutex_unlock(&localtime_lock);

net-mgmt/netmond/files/patch-netmon.h
@@ -1,176 +1,0 @@
---- netmon.h.orig	Tue Aug 26 10:00:38 2003
-+++ netmon.h	Thu Dec 28 13:31:06 2006
-@@ -14,6 +14,9 @@
- #include <sys/socket.h>
- #include <sys/time.h>
- #include <netinet/in.h>
-+#include <pwd.h>
-+#include <grp.h>
-+#include <time.h>
- #ifdef	DEBUG_MEMORY
- #include <assert.h>
- #endif
-@@ -77,7 +80,10 @@
- #endif
- 
- #define	NETMON			"netmon"
--#define	DEFAULT_CONFIG		"/etc/netmon.conf"
-+#define	DEFAULT_CONFIG		"/usr/local/etc/netmond.conf"
-+#define USERNAME		"netmon"
-+#define GROUPNAME		"netmon"
-+#define PIDFILE_PATH		"/var/run"
- #define	DEFAULT_WATCHDOG	600	/* 10 min */
- 
- #define	POLLING_MIN		30	/* 30 sec */
-@@ -88,9 +94,13 @@
- typedef	struct timeval TIMEVAL;
- typedef	unsigned long oid;
- 
-+#define	STATE_UNKNOWN	0
- #define	STATE_UP	1
- #define	STATE_DOWN	2
-+#define	STATE_DEGRADED	3
-+#define	STATE_WARNING	4
- #define	BGP_ESTABLISHED	6
-+#define	ENV_NORMAL	1
- #define	ENV_NOTPRESENT	5
- 
- #define	OBJ_STATE	0
-@@ -111,6 +121,9 @@
- #define	TYPE_ENVFAN		9
- #define	TYPE_ENVPS		10
- 
-+#define WHEN_PROTO		10099
-+#define PIPE_PROTO		10098
-+
- struct object_ent;
- struct method_ent;
- 
-@@ -250,6 +263,7 @@
- 
- 	int state;		/* current operational status (UP/DOWN/...) */
- 	int prev_state;		/* previous operational status */
-+	int ignore_state;       /* if =1, ignore this interface while parent object state evaluating */
- 	TIMEVAL last_request;	/* last time interface requested */
- 	TIMEVAL prev_request;	/* previous time interface requested */
- 	TIMEVAL last_reply;	/* last time interface reply */
-@@ -294,6 +308,7 @@
- 	int asn;		/* AS number */
- 	char *descr;		/* AS description */
- 	char *datadir;		/* directory where store data */
-+	int ignore_state;       /* if =1, ignore this AS while parent object state evaluating */
- 	SAVE *save_list;	/* list of save methods */
- 	GROUP_REF *ns_acl;	/* netstate client access list */
- 
-@@ -325,6 +340,7 @@
- 	int type;		/* type of gauge: TYPE_ENVTEMP, etc */
- 	char *descr;		/* env mon description */
- 	char *datadir;		/* directory where store data */
-+	int ignore_state;       /* if =1, ignore this ENV while parent object state evaluating */
- 	SAVE *save_list;	/* list of save methods */
- 	GROUP_REF *ns_acl;	/* netstate client access list */
- 
-@@ -385,13 +401,14 @@
- 	struct method_ent *method;	/* session method */
- 	int sock;		/* socket file descriptor */
- 	struct sockaddr peer;	/* address of peer */
-+	struct sockaddr me;	/* my source address  */
- 	long timeout;		/* number of microseconds until first timeout */
- 	int retries;		/* number of retries before timeout */
- 	int (*connect) __P((struct session_ent *));
- 	int (*send) __P((struct session_ent *, REQUEST *));
- 	int (*recv) __P((struct session_ent *));
- 	void (*read) __P((int, struct session_ent *, int));
--
-+	pid_t	pid;
- 	/* returned values */
- 	int data_int;		/* data length or chat-script matchs */
- 	char *data_ptr;		/* pointer to resulting data if any */
-@@ -428,6 +445,8 @@
- 	u_short	rport;		/* remote port number, 0=unused */
- 	int timeout;		/* number of seconds until first timeout */
- 	int retries;		/* number of retries before timeout */
-+	char *when;	        /* condition string */
-+	char *when_fmt;         /* message when condition is true */
- 	union {
- 		struct ping_param {
- 			short send;	/* ICMP echo request packets to send */
-@@ -530,7 +549,9 @@
- 	char *descr;		/* object description */
- 	char *datadir;		/* directory where store data */
- 	char *address;		/* domain name or dotted IP address */
-+	char *srcaddress;	/* domain name or dotted source IP address */
- 	struct in_addr ip_addr;	/* ip address of peer */
-+	struct in_addr ip_srcaddr; /* source ip address */
- 	int polling;		/* polling period in seconds */
- 	int saving;		/* saving period in seconds */
- 	int sync;		/* polling counter to synchronize saving */
-@@ -544,6 +565,10 @@
- 
- 	int state;		/* current operational status (UP/DOWN/...) */
- 	int prev_state;		/* previous operational status */
-+	int ignore_state;       /* if =1, ignore this obj while parent object state evaluating */
-+	int mths_ok;		/* count of Ok finished methods */
-+	int mths_fail;		/* count of Failed methods */
-+	int smths_fail;		/* count of Failed services methods */
- 	TIMEVAL last_request;	/* last time method requested */
- 	TIMEVAL prev_request;	/* previous time method requested */
- 	TIMEVAL last_reply;	/* last time method reply */
-@@ -574,17 +599,29 @@
- 
- typedef	struct config_ent {
- 	char *rootdir;		/* default work directory */
-+	char *chrootdir;	/* chroot directory for EXEC children */
-+	char *username;		/* username for EXEC children */	
-+	uid_t	uid;		/* UID for EXEC children */	
-+	char *groupname;	/* groupname for EXEC children */	
-+	gid_t	gid;		/* GID for EXEC children */	
- 	char *timefmt;		/* strftime format of currtime for logging */
-+	char *srcaddress;	/* my default source domain name or dotted IP address */
-+	struct in_addr ip_srcaddr; /* my default sorce ip address  */
- 	int polling;		/* default polling interval in seconds */
- 	int saving;		/* default saving interval in seconds */
- 	int timeout;		/* default timeout in seconds */
- 	int retries;		/* default number of retries */
-+	int compatibility_flag;	/*  */
- 
- 	int enable_traps;	/* enable SNMP traps */
- 	int source_traps;	/* match src-addr and agent-addr of traps */
-+	char *trap_address;	/* Trap bind address */
-+	struct in_addr trap_ip_addr;	/*  */	
- 
- 	/* netstate server */
- 	int ns_port;		/* server port number */
-+	char *ns_address;	/* NetState bind address */
-+	struct in_addr ns_ip_addr;	/*  */	
- 	int ns_timo;		/* client timeout in seconds */
- 	GROUP_REF *ns_acl;	/* netstate client access list */
- 
-@@ -616,6 +653,7 @@
- extern int netstate_sock;
- extern int Cflag;
- extern int syslog_initialized;
-+extern int compatibility_flag;
- 
- /*
-  * Function prototypes
-@@ -733,6 +771,19 @@
- void tcp_start __P((METHOD *));
- void tcp_stop __P((METHOD *));
- int match_expect __P((SESSION *, CHATSCRIPT *, char *));
-+int tcp_connect __P((SESSION *));
-+int tcp_send __P((SESSION *,REQUEST *));
-+int tcp_recv __P((SESSION *));
-+void tcp_close __P((int, SESSION *, int));
-+
-+/* pipe.c */
-+int pipe_init __P((OBJECT *, METHOD *));
-+void pipe_start __P((METHOD *));
-+void pipe_stop __P((METHOD *));
-+
-+int when_init __P((OBJECT *, METHOD *));
-+void when_start __P((METHOD *));
-+void when_stop __P((METHOD *));
- 
- /* udp.c */
- int udp_init __P((OBJECT *, METHOD *));

commit 21434d04412a4cea38317e499b8c5c8b9767e50f

Fix @dirrm auto-generation for PORTDOCS/PORTEXAMPLES/PORTDATA with globs.
Fix up net-mgmt/netmond pkg-plist for @mode (regression with pkg from
r347404 committed by rene@) and remove two @dirrm lines that are now excess.

Exp-run completed.

PR:		191746
Reviewed by:	antoine
Approved by:	portmgr (antoine)


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@366361 35697150-7ecd-e111-bb59-0022644237b5

Mk/bsd.port.mk
@@ -645,6 +645,10 @@ FreeBSD_MAINTAINER=	portmgr@FreeBSD.org
 #				  Should not be set when no documentation files are
 #				  installed.
 #				  Useful for dynamically generated documentation.
+#				  NOTE: this may fail to generate @dirrm entries for
+#				  complex patterns. In such a case, please abstain from
+#				  using DOCSDIR and add files and @dirrm-directories to
+#				  pkg-plist instead (see make makeplist).
 #
 # Set the following to specify all documentation your port installs into
 # ${EXAMPLESDIR}
@@ -5555,7 +5559,7 @@ add-plist-docs:
 .endfor
 	@${FIND} -P ${PORTDOCS:S/^/${STAGEDIR}${DOCSDIR}\//} ! -type d 2>/dev/null | \
 		${SED} -ne 's,^${STAGEDIR}${PREFIX}/,,p' >> ${TMPPLIST}
-	@${FIND} -P -d ${PORTDOCS:S/^/${STAGEDIR}${DOCSDIR}\//} -type d 2>/dev/null | \
+	@${FIND} -P -d ${PORTDOCS:S/^/${STAGEDIR}${DOCSDIR}\//:C,/[^/]*[*?\[][^/]*$,,} -type d 2>/dev/null | \
 		${SED} -ne 's,^${STAGEDIR}${PREFIX}/,@dirrm ,p' >> ${TMPPLIST}
 	@${ECHO_CMD} "@dirrm ${DOCSDIR_REL}" >> ${TMPPLIST}
 .endif
@@ -5577,7 +5581,7 @@ add-plist-examples:
 .endfor
 	@${FIND} -P ${PORTEXAMPLES:S/^/${STAGEDIR}${EXAMPLESDIR}\//} ! -type d 2>/dev/null | \
 		${SED} -ne 's,^${STAGEDIR}${PREFIX}/,,p' >> ${TMPPLIST}
-	@${FIND} -P -d ${PORTEXAMPLES:S/^/${STAGEDIR}${EXAMPLESDIR}\//} -type d 2>/dev/null | \
+	@${FIND} -P -d ${PORTEXAMPLES:C,/[^/]*[*?\[][^/]*$,,:S/^/${STAGEDIR}${EXAMPLESDIR}\//} -type d 2>/dev/null | \
 		${SED} -ne 's,^${STAGEDIR}${PREFIX}/,@dirrm ,p' >> ${TMPPLIST}
 	@${ECHO_CMD} "@dirrm ${EXAMPLESDIR_REL}" >> ${TMPPLIST}
 .endif
@@ -5599,7 +5603,7 @@ add-plist-data:
 .endfor
 	@${FIND} -P ${PORTDATA:S/^/${STAGEDIR}${DATADIR}\//} ! -type d 2>/dev/null | \
 		${SED} -ne 's,^${STAGEDIR}${PREFIX}/,,p' >> ${TMPPLIST}
-	@${FIND} -P -d ${PORTDATA:S/^/${STAGEDIR}${DATADIR}\//} -type d 2>/dev/null | \
+	@${FIND} -P -d ${PORTDATA:C,/[^/]*[*?\[][^/]*$,,:S/^/${STAGEDIR}${DATADIR}\//} -type d 2>/dev/null | \
 		${SED} -ne 's,^${STAGEDIR}${PREFIX}/,@dirrm ,p' >> ${TMPPLIST}
 	@${ECHO_CMD} "@dirrm ${DATADIR_REL}" >> ${TMPPLIST}
 .endif

net-mgmt/netmond/Makefile
@@ -3,7 +3,7 @@
 
 PORTNAME=	netmond
 PORTVERSION=	2.2b6
-PORTREVISION=	6
+PORTREVISION=	7
 CATEGORIES=	net-mgmt
 MASTER_SITES=	http://freebsd.unixfreunde.de/sources/:daemon \
 		ftp://ftp.risp.ru/pub/RinetSoftware/:daemon \

net-mgmt/netmond/pkg-plist
@@ -3,10 +3,8 @@
 %%GD%%sbin/ifgraph
 sbin/netmond
 sbin/netmond_watchdog
-@mode u+s
+@mode 04555
 sbin/netmondctl
 @mode
 @group wheel
 etc/netmond.conf.sample
-%%PORTDOCS%%@dirrmtry %%DOCSDIR%%/ru
-%%PORTDOCS%%@dirrmtry %%DOCSDIR%%/en