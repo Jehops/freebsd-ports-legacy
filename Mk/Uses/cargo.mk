Jehops/freebsd-ports-legacy

commit 2bd6c86ecfaf133f2edd2d052ee147b13e988712

www/geckodriver: Update to 0.24.0
PR:		239260
Submitted by:	Daniel Ylitalo <daniel@blodan.se> (maintainer)


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@506813 35697150-7ecd-e111-bb59-0022644237b5

www/geckodriver/Makefile
@@ -2,8 +2,7 @@
 
 PORTNAME=	geckodriver
 DISTVERSIONPREFIX=	v
-DISTVERSION=	0.23.0
-PORTREVISION=	9
+DISTVERSION=	0.24.0
 CATEGORIES=	www
 
 MAINTAINER=	daniel@blodan.se
@@ -17,140 +16,318 @@ GH_ACCOUNT=	mozilla
 
 PLIST_FILES=	bin/geckodriver
 
-CARGO_CRATES=	adler32-1.0.3 \
+CARGO_CRATES=	Inflector-0.11.2 \
+		adler32-1.0.2 \
 		aho-corasick-0.6.8 \
-		arrayvec-0.4.7 \
-		base64-0.9.3 \
+		ansi_term-0.11.0 \
+		app_units-0.7.0 \
+		argon2rs-0.2.5 \
+		arrayref-0.3.4 \
+		arrayvec-0.4.6 \
+		ascii-canvas-1.0.0 \
+		atomic_refcell-0.1.0 \
+		atty-0.2.11 \
+		backtrace-0.3.9 \
+		backtrace-sys-0.1.24 \
+		base64-0.10.0 \
+		binary-space-partition-0.1.2 \
+		bincode-1.0.0 \
+		bindgen-0.43.0 \
+		binjs_meta-0.4.3 \
+		bit-set-0.5.0 \
+		bit-vec-0.5.0 \
 		bitflags-1.0.4 \
-		build_const-0.2.1 \
-		byteorder-1.2.6 \
-		bytes-0.4.10 \
-		bzip2-0.3.3 \
+		bitreader-0.3.0 \
+		blake2-rfc-0.2.18 \
+		block-buffer-0.3.3 \
+		block-buffer-0.7.0 \
+		block-padding-0.1.2 \
+		boxfnonce-0.0.3 \
+		build_const-0.2.0 \
+		byte-tools-0.2.0 \
+		byte-tools-0.3.0 \
+		byteorder-1.2.7 \
+		bytes-0.4.9 \
+		bzip2-0.3.2 \
 		bzip2-sys-0.1.6 \
-		cc-1.0.25 \
-		cfg-if-0.1.5 \
+		cast-0.2.2 \
+		cc-1.0.23 \
+		cexpr-0.3.3 \
+		cfg-if-0.1.2 \
 		chrono-0.4.6 \
-		clap-2.32.0 \
-		cloudabi-0.0.3 \
+		clang-sys-0.26.1 \
+		clap-2.31.2 \
+		cmake-0.1.29 \
+		constant_time_eq-0.1.3 \
 		cookie-0.11.0 \
-		crc-1.8.1 \
-		crossbeam-deque-0.6.1 \
-		crossbeam-epoch-0.5.2 \
-		crossbeam-utils-0.5.0 \
-		flate2-1.0.2 \
+		core-foundation-0.6.3 \
+		core-foundation-sys-0.6.2 \
+		core-graphics-0.17.1 \
+		core-text-13.0.0 \
+		cose-0.1.4 \
+		cose-c-0.1.5 \
+		cranelift-bforest-0.28.0 \
+		cranelift-codegen-0.28.0 \
+		cranelift-codegen-meta-0.28.0 \
+		cranelift-entity-0.28.0 \
+		cranelift-frontend-0.28.0 \
+		cranelift-wasm-0.28.0 \
+		crc-1.7.0 \
+		crossbeam-deque-0.2.0 \
+		crossbeam-deque-0.3.1 \
+		crossbeam-epoch-0.3.1 \
+		crossbeam-epoch-0.4.3 \
+		crossbeam-utils-0.2.2 \
+		crossbeam-utils-0.3.2 \
+		cssparser-0.25.0 \
+		cssparser-macros-0.3.3 \
+		cstr-0.1.3 \
+		cstr-macros-0.1.3 \
+		cubeb-0.5.2 \
+		cubeb-backend-0.5.0 \
+		cubeb-core-0.5.1 \
+		cubeb-sys-0.5.1 \
+		darling-0.8.1 \
+		darling_core-0.8.1 \
+		darling_macro-0.8.1 \
+		derive_more-0.13.0 \
+		devd-rs-0.2.1 \
+		diff-0.1.11 \
+		digest-0.7.6 \
+		digest-0.8.0 \
+		dirs-1.0.4 \
+		docopt-0.8.3 \
+		dtoa-0.4.2 \
+		dtoa-short-0.3.1 \
+		dwrote-0.8.0 \
+		either-1.1.0 \
+		ena-0.9.3 \
+		encoding_c-0.9.0 \
+		encoding_rs-0.8.14 \
+		env_logger-0.5.6 \
+		error-chain-0.11.0 \
+		euclid-0.19.5 \
+		euclid_macros-0.1.0 \
+		failure-0.1.3 \
+		failure_derive-0.1.3 \
+		fake-simd-0.1.2 \
+		fixedbitset-0.1.8 \
+		flate2-1.0.1 \
 		fnv-1.0.6 \
+		foreign-types-0.3.0 \
+		freetype-0.4.0 \
+		fs2-0.4.3 \
 		fuchsia-zircon-0.3.3 \
 		fuchsia-zircon-sys-0.3.3 \
-		futures-0.1.24 \
+		futures-0.1.23 \
 		futures-cpupool-0.1.8 \
+		fxhash-0.2.1 \
+		gcc-0.3.54 \
+		generic-array-0.9.0 \
+		generic-array-0.12.0 \
+		gl_generator-0.10.0 \
+		gleam-0.6.8 \
+		glob-0.2.11 \
+		goblin-0.0.17 \
 		h2-0.1.12 \
-		http-0.1.13 \
-		httparse-1.3.2 \
-		hyper-0.12.10 \
-		idna-0.1.5 \
+		http-0.1.10 \
+		httparse-1.2.3 \
+		humantime-1.1.1 \
+		hyper-0.12.7 \
+		ident_case-1.0.0 \
+		idna-0.1.4 \
 		indexmap-1.0.1 \
 		iovec-0.1.2 \
-		itoa-0.4.3 \
+		itertools-0.7.6 \
+		itoa-0.4.1 \
 		kernel32-sys-0.2.2 \
-		lazy_static-1.1.0 \
-		lazycell-1.1.0 \
-		libc-0.2.49 \
-		lock_api-0.1.3 \
-		log-0.4.5 \
-		matches-0.1.8 \
-		memchr-2.0.2 \
+		khronos_api-3.0.0 \
+		lalrpop-0.16.0 \
+		lalrpop-snap-0.16.0 \
+		lalrpop-util-0.16.0 \
+		lazy_static-1.0.1 \
+		lazycell-0.4.0 \
+		lazycell-0.6.0 \
+		libc-0.2.43 \
+		libloading-0.5.0 \
+		libudev-0.2.0 \
+		libudev-sys-0.1.3 \
+		libz-sys-1.0.16 \
+		linked-hash-map-0.5.1 \
+		lmdb-rkv-0.8.2 \
+		lmdb-sys-0.8.0 \
+		lock_api-0.1.5 \
+		log-0.3.9 \
+		log-0.4.6 \
+		malloc_size_of_derive-0.1.0 \
+		matches-0.1.6 \
+		memchr-1.0.2 \
+		memchr-2.0.1 \
+		memmap-0.5.2 \
+		memmap-0.6.2 \
 		memoffset-0.2.1 \
-		miniz_oxide-0.1.3 \
-		miniz_oxide_c_api-0.1.3 \
-		mio-0.6.16 \
-		mio-uds-0.6.7 \
+		miniz_oxide-0.1.2 \
+		miniz_oxide_c_api-0.1.2 \
+		mio-0.6.15 \
+		mio-uds-0.6.4 \
 		miow-0.2.1 \
-		mozprofile-0.4.0 \
-		mozrunner-0.8.0 \
-		mozversion-0.1.3 \
+		moz_cbor-0.1.1 \
+		mozprofile-0.5.0 \
+		mozrunner-0.9.0 \
+		mozversion-0.2.0 \
+		mp4parse-0.11.2 \
+		mp4parse_capi-0.11.2 \
+		mp4parse_fallible-0.0.1 \
 		msdos_time-0.1.6 \
-		net2-0.2.33 \
+		net2-0.2.32 \
+		new_debug_unreachable-1.0.1 \
 		nodrop-0.1.12 \
-		num-0.1.42 \
+		nom-3.2.1 \
+		nom-4.1.1 \
+		num-derive-0.2.2 \
 		num-integer-0.1.39 \
-		num-iter-0.1.37 \
+		num-traits-0.1.43 \
 		num-traits-0.2.6 \
-		num_cpus-1.8.0 \
-		owning_ref-0.3.3 \
-		parking_lot-0.6.4 \
-		parking_lot_core-0.3.1 \
-		percent-encoding-1.0.1 \
-		podio-0.1.6 \
-		proc-macro2-0.4.19 \
-		quote-0.6.8 \
+		num_cpus-1.7.0 \
+		object-0.10.0 \
+		opaque-debug-0.2.1 \
+		ordered-float-1.0.1 \
+		ordermap-0.3.5 \
+		owning_ref-0.4.0 \
+		parking_lot-0.6.3 \
+		parking_lot_core-0.2.14 \
+		peeking_take_while-0.1.2 \
+		percent-encoding-1.0.0 \
+		petgraph-0.4.13 \
+		phf-0.7.21 \
+		phf_codegen-0.7.21 \
+		phf_generator-0.7.21 \
+		phf_shared-0.7.21 \
+		pkg-config-0.3.9 \
+		plain-0.2.3 \
+		plane-split-0.13.3 \
+		podio-0.1.5 \
+		precomputed-hash-0.1.1 \
+		proc-macro2-0.3.5 \
+		proc-macro2-0.4.24 \
+		procedural-masquerade-0.1.1 \
+		pulse-0.2.0 \
+		quick-error-1.2.1 \
+		quote-0.5.2 \
+		quote-0.6.10 \
 		rand-0.3.22 \
 		rand-0.4.3 \
-		rand-0.5.5 \
-		rand_core-0.2.1 \
-		redox_syscall-0.1.40 \
-		regex-0.2.11 \
-		regex-1.0.5 \
-		regex-syntax-0.5.6 \
-		regex-syntax-0.6.2 \
-		remove_dir_all-0.5.1 \
+		rayon-1.0.0 \
+		rayon-core-1.4.0 \
+		redox_syscall-0.1.32 \
+		redox_termios-0.1.1 \
+		redox_users-0.2.0 \
+		regex-0.2.2 \
+		regex-1.0.0 \
+		regex-syntax-0.4.1 \
+		regex-syntax-0.6.0 \
+		rkv-0.5.1 \
+		ron-0.1.7 \
+		runloop-0.1.0 \
 		rust-ini-0.10.3 \
+		rustc-demangle-0.1.8 \
 		rustc_version-0.2.3 \
-		ryu-0.2.6 \
-		safemem-0.3.0 \
-		scopeguard-0.3.3 \
+		ryu-0.2.4 \
+		same-file-1.0.2 \
+		scoped-tls-0.1.0 \
+		scoped_threadpool-0.1.9 \
+		scopeguard-0.3.2 \
+		scroll-0.9.2 \
+		scroll_derive-0.9.5 \
+		selectors-0.21.0 \
 		semver-0.6.0 \
 		semver-0.9.0 \
 		semver-parser-0.7.0 \
-		serde-1.0.79 \
-		serde_derive-1.0.79 \
-		serde_json-1.0.27 \
+		serde-1.0.80 \
+		serde_bytes-0.10.4 \
+		serde_derive-1.0.80 \
+		serde_json-1.0.26 \
+		servo_arc-0.1.1 \
+		sha2-0.7.1 \
+		sha2-0.8.0 \
+		simd-0.2.3 \
+		siphasher-0.2.1 \
+		slab-0.3.0 \
 		slab-0.4.1 \
-		smallvec-0.6.5 \
-		stable_deref_trait-1.1.1 \
+		smallbitvec-2.3.0 \
+		smallvec-0.6.6 \
+		stable_deref_trait-1.0.0 \
 		string-0.1.1 \
+		string_cache-0.7.3 \
+		string_cache_codegen-0.4.2 \
+		string_cache_shared-0.3.0 \
+		strsim-0.6.0 \
 		strsim-0.7.0 \
-		syn-0.15.4 \
-		tempdir-0.3.7 \
-		term_size-0.3.1 \
-		textwrap-0.10.0 \
-		thread_local-0.3.6 \
+		syn-0.13.1 \
+		syn-0.14.6 \
+		syn-0.15.24 \
+		synstructure-0.10.1 \
+		target-lexicon-0.2.0 \
+		tempdir-0.3.5 \
+		term-0.4.6 \
+		term_size-0.3.0 \
+		termcolor-0.3.6 \
+		termion-1.5.1 \
+		textwrap-0.9.0 \
+		thin-slice-0.1.1 \
+		thin-vec-0.1.0 \
+		thread_local-0.3.5 \
+		thread_profiler-0.1.1 \
 		time-0.1.40 \
-		tokio-0.1.8 \
+		tokio-0.1.7 \
 		tokio-codec-0.1.0 \
-		tokio-current-thread-0.1.1 \
-		tokio-executor-0.1.4 \
+		tokio-core-0.1.17 \
+		tokio-executor-0.1.3 \
 		tokio-fs-0.1.3 \
-		tokio-io-0.1.8 \
-		tokio-reactor-0.1.5 \
+		tokio-io-0.1.7 \
+		tokio-reactor-0.1.3 \
 		tokio-tcp-0.1.1 \
-		tokio-threadpool-0.1.6 \
-		tokio-timer-0.2.6 \
-		tokio-udp-0.1.2 \
-		tokio-uds-0.2.1 \
+		tokio-threadpool-0.1.5 \
+		tokio-timer-0.2.5 \
+		tokio-udp-0.1.1 \
+		tokio-uds-0.1.7 \
+		toml-0.4.5 \
 		try-lock-0.2.2 \
+		typenum-1.10.0 \
 		ucd-util-0.1.1 \
+		uluru-0.3.0 \
 		unicode-bidi-0.3.4 \
-		unicode-normalization-0.1.7 \
+		unicode-normalization-0.1.5 \
 		unicode-segmentation-1.2.1 \
-		unicode-width-0.1.5 \
+		unicode-width-0.1.4 \
 		unicode-xid-0.1.0 \
 		unreachable-1.0.0 \
-		url-1.7.1 \
-		utf8-ranges-1.0.1 \
-		uuid-0.5.1 \
-		version_check-0.1.5 \
+		url-1.7.2 \
+		utf8-ranges-1.0.0 \
+		uuid-0.6.5 \
+		vcpkg-0.2.2 \
+		vec_map-0.8.0 \
 		void-1.0.2 \
+		walkdir-2.1.4 \
 		want-0.0.6 \
-		webdriver-0.38.0 \
+		wasmparser-0.23.0 \
+		webdriver-0.39.0 \
+		webidl-0.8.0 \
+		webrender_api-0.58.0 \
+		webrender_build-0.0.1 \
+		which-1.0.3 \
 		winapi-0.2.8 \
-		winapi-0.3.5 \
+		winapi-0.3.6 \
 		winapi-build-0.1.1 \
 		winapi-i686-pc-windows-gnu-0.4.0 \
 		winapi-x86_64-pc-windows-gnu-0.4.0 \
+		wincolor-0.1.6 \
 		winreg-0.5.1 \
+		wr_malloc_size_of-0.0.1 \
 		ws2_32-sys-0.2.1 \
-		zip-0.4.0
-
+		xml-rs-0.8.0 \
+		yaml-rust-0.4.2 \
+		zip-0.4.2
 post-install:
 	@${STRIP_CMD} ${STAGEDIR}${PREFIX}/bin/geckodriver
 

www/geckodriver/pkg-descr
@@ -1,6 +1,6 @@
-This program provides the HTTP API described by the 
-WebDriver protocol to communicate with Gecko browsers, such as Firefox. 
-It translates calls into the Marionette automation protocol by acting 
+This program provides the HTTP API described by the
+WebDriver protocol to communicate with Gecko browsers, such as Firefox.
+It translates calls into the Marionette automation protocol by acting
 as a proxy between the local- and remote ends.
 
 WWW: https://github.com/mozilla/geckodriver

commit 64e9125792a3b1abfc2e4e10a758de924c3bed05

www/geckodriver: Bump libc crate version again
$ make check-sanity
/!\ geckodriver-0.24.0: Makefile warnings, please consider fixing /!\

CARGO_CRATES=libc-0.2.43 may be unstable on aarch64 or not build on armv6,
armv7, powerpc64. Consider updating to the latest version (higher than 0.2.49).

This will give it at least a chance to build on tier 2 archs.  We
bumped libc earlier but this was regressed on in r506813.

The libc warnings in USES=cargo do not seem to be enough.  Make
them errors instead.

With hat:	rust


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@506841 35697150-7ecd-e111-bb59-0022644237b5

Mk/Uses/cargo.mk
@@ -139,10 +139,10 @@ CARGO_ENV+=	GETTEXT_BIN_DIR=${LOCALBASE}/bin \
 # https://github.com/rust-lang/libc/commit/969ad2b73cdc
 _libc_VER=	${libc:C/.*-//}
 . if ${_libc_VER:R:R} == 0 && (${_libc_VER:R:E} < 2 || ${_libc_VER:R:E} == 2 && ${_libc_VER:E} < 38)
-DEV_WARNING+=	"CARGO_CRATES=${libc} may be unstable on FreeBSD 12.0. Consider updating to the latest version (higher than 0.2.37)."
+DEV_ERROR+=	"CARGO_CRATES=${libc} may be unstable on FreeBSD 12.0. Consider updating to the latest version \(higher than 0.2.37\)."
 . endif
 . if ${_libc_VER:R:R} == 0 && (${_libc_VER:R:E} < 2 || ${_libc_VER:R:E} == 2 && ${_libc_VER:E} < 49)
-DEV_WARNING+=	"CARGO_CRATES=${libc} may be unstable on aarch64 or not build on armv6, armv7, powerpc64. Consider updating to the latest version (higher than 0.2.49)."
+DEV_ERROR+=	"CARGO_CRATES=${libc} may be unstable on aarch64 or not build on armv6, armv7, powerpc64. Consider updating to the latest version \(higher than 0.2.49\)."
 . endif
 .undef _libc_VER
 .endfor

www/geckodriver/Makefile
@@ -3,6 +3,7 @@
 PORTNAME=	geckodriver
 DISTVERSIONPREFIX=	v
 DISTVERSION=	0.24.0
+PORTREVISION=	1
 CATEGORIES=	www
 
 MAINTAINER=	daniel@blodan.se
@@ -148,7 +149,7 @@ CARGO_CRATES=	Inflector-0.11.2 \
 		lazy_static-1.0.1 \
 		lazycell-0.4.0 \
 		lazycell-0.6.0 \
-		libc-0.2.43 \
+		libc-0.2.60 \
 		libloading-0.5.0 \
 		libudev-0.2.0 \
 		libudev-sys-0.1.3 \

www/geckodriver/distinfo
@@ -1,4 +1,4 @@
-TIMESTAMP = 1563353187
+TIMESTAMP = 1563436381
 SHA256 (rust/crates/Inflector-0.11.2.tar.gz) = 1b33cd9b653730fc539c53c7b3c672d2f47108fa20c6df571fa5817178f5a14c
 SIZE (rust/crates/Inflector-0.11.2.tar.gz) = 23756
 SHA256 (rust/crates/adler32-1.0.2.tar.gz) = 6cbd0b9af8587c72beadc9f72d35b9fbb070982c9e6203e46e93f10df25f8f45
@@ -263,8 +263,8 @@ SHA256 (rust/crates/lazycell-0.4.0.tar.gz) = ce12306c4739d86ee97c23139f3a34ddf03
 SIZE (rust/crates/lazycell-0.4.0.tar.gz) = 8456
 SHA256 (rust/crates/lazycell-0.6.0.tar.gz) = a6f08839bc70ef4a3fe1d566d5350f519c5912ea86be0df1740a7d247c7fc0ef
 SIZE (rust/crates/lazycell-0.6.0.tar.gz) = 10099
-SHA256 (rust/crates/libc-0.2.43.tar.gz) = 76e3a3ef172f1a0b9a9ff0dd1491ae5e6c948b94479a3021819ba7d860c8645d
-SIZE (rust/crates/libc-0.2.43.tar.gz) = 353810
+SHA256 (rust/crates/libc-0.2.60.tar.gz) = d44e80633f007889c7eff624b709ab43c92d708caad982295768a7b13ca3b5eb
+SIZE (rust/crates/libc-0.2.60.tar.gz) = 409744
 SHA256 (rust/crates/libloading-0.5.0.tar.gz) = 9c3ad660d7cb8c5822cd83d10897b0f1f1526792737a179e73896152f85b88c2
 SIZE (rust/crates/libloading-0.5.0.tar.gz) = 15990
 SHA256 (rust/crates/libudev-0.2.0.tar.gz) = ea626d3bdf40a1c5aee3bcd4f40826970cae8d80a8fec934c82a63840094dcfe