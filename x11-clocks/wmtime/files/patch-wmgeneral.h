Jehops/freebsd-ports-legacy

commit ef36878e8fad3558290df7fa266b4080256983f8

x11-clocks/wmtime: Fix build and deprecate
Fix the build by adding -fcommon to cflags, and deprecate since this
software is not maintained and there is no upstream.

MFH:		2020Q3 (implicit, -fno-common fixes)


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@546907 35697150-7ecd-e111-bb59-0022644237b5

x11-clocks/wmtime/Makefile
@@ -12,6 +12,12 @@ MASTER_SITES=	http://web.cs.mun.ca/~gstarkes/wmaker/dockapps/files/ \
 MAINTAINER=	ports@FreeBSD.org
 COMMENT=	Time/Date applet for WindowMaker
 
+# Fix build with llvm 11
+CFLAGS+=	-fcommon
+
+DEPRECATED=	Unmaintained and dead upstream
+EXPIRATION_DATE=2020-09-30
+
 WRKSRC=		${WRKDIR}/wmtime.app/wmtime
 USES=		imake xorg
 USE_XORG=	xpm

commit 0d1ce182efdd5242f66254f365f2271780bb1deb

Patch source code to fix the problem -fno-common tripped over
Along the way take maintainership.  I have a soft spot for WM

Fixing this was faster than replying to the thread caused by r546907


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@546968 35697150-7ecd-e111-bb59-0022644237b5

x11-clocks/wmtime/Makefile
@@ -3,20 +3,14 @@
 
 PORTNAME=	wmtime
 PORTVERSION=	1.0b2
-PORTREVISION=	1
+PORTREVISION=	2
 CATEGORIES=	x11-clocks windowmaker
 MASTER_SITES=	http://web.cs.mun.ca/~gstarkes/wmaker/dockapps/files/ \
 		http://freebsd.unixfreunde.de/sources/ \
 		http://mirror2.unixfreunde.de/
 
-MAINTAINER=	ports@FreeBSD.org
+MAINTAINER=	jpaetzel@FreeBSD.org
 COMMENT=	Time/Date applet for WindowMaker
-
-# Fix build with llvm 11
-CFLAGS+=	-fcommon
-
-DEPRECATED=	Unmaintained and dead upstream
-EXPIRATION_DATE=2020-09-30
 
 WRKSRC=		${WRKDIR}/wmtime.app/wmtime
 USES=		imake xorg

x11-clocks/wmtime/files/patch-wmgeneral.h
@@ -1,0 +1,11 @@
+--- ../wmgeneral/wmgeneral.h.orig	2020-08-29 19:54:52.707315000 -0700
++++ ../wmgeneral/wmgeneral.h	2020-08-29 19:55:08.284120000 -0700
+@@ -28,7 +28,7 @@
+  /* Global variable */
+ /*******************/
+ 
+-Display		*display;
++extern Display		*display;
+ 
+   /***********************/
+  /* Function Prototypes */

x11-clocks/wmtime/files/patch-wmtime.c
@@ -1,5 +1,5 @@
---- wmtime.c	Wed May 20 06:34:53 1998
-+++ wmtime.c2	Sat Nov 28 18:44:59 1998
+--- wmtime.c.orig	1998-05-19 21:04:53 UTC
++++ wmtime.c
 @@ -89,7 +89,7 @@
  char	*ProgName;
  int		digital = 0;
@@ -9,3 +9,11 @@
  
  /* functions */
  void usage(void);
+@@ -166,6 +166,7 @@ void get_lang(){
+ char		*left_action = NULL;
+ char		*right_action = NULL;
+ char		*middle_action = NULL;
++Display		*display;
+ 
+ void DrawTime(int, int, int);
+ void DrawWijzer(int, int, int);